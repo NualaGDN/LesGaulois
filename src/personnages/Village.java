de1121471ba18e40885e8b7791c9e3f25faaad2b
NualaGDN <108536925+NualaGDN@users.noreply.github.com>
2023-03-14 17:44:08 +0100

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,22 @@
 package personnages;
 
 public class Village {
+	private String nom;
+	private Chef chef;
+	private Villageois[] Villageois;
+	private int nbVillageois = 0;
+	
+	
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
 
 }
